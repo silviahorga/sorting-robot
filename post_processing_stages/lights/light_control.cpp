5007972ed5651d8e3ed01cd1bb5a12fddeaab9d7
silviahorga <167780606+silviahorga@users.noreply.github.com>
2024-05-22 12:36:17 +0200

    Update light_control.cpp

--- post_processing_stages/lights/light_control.cpp
+++ post_processing_stages/lights/light_control.cpp
@@ -1,15 +1,27 @@
 // light_control.cpp
 #include "light_control.hpp"
 #include <iostream>
+#include <csignal>
+#include <iostream>
+#include <pigpio.h>
+const int LED = 21;
 
 void lightOn()
 {
 	std::cout << "Light is turned ON" << std::endl;
 	// Add your hardware-specific code here to turn the light on
+	if (gpioInitialise() == PI_INIT_FAILED) {
+      		std::cout << "ERROR: Failed to initialize the GPIO interface." << std::endl;
+      	return 1;
+   	}
+	gpioSetMode(LED, PI_OUTPUT);
+	gpioWrite(LED, PI_HIGH);
 }
 
 void lightOff()
 {
 	std::cout << "Light is turned OFF" << std::endl;
 	// Add your hardware-specific code here to turn the light off
+	gpioSetMode(LED, PI_OUTPUT);
+	gpioWrite(LED, PI_LOW);
 }
